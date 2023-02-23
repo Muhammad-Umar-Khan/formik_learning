d586aef2a9219d09c263a01b2a6ba43be14bbb4d
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-02-23 12:44:51 +0500

    form inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,17 @@
+import * as Yup from "yup";
+import { Formik, Form, Field } from "formik";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("Required"),
+  username: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email address").required("Required"),
+  address: Yup.object({
+    street: Yup.string().required("Required"),
+    suite: Yup.string().required("Required"),
+    city: Yup.string().required("Required"),
+  }),
+});
+
 const Form = () => {
   return (
     <form>
